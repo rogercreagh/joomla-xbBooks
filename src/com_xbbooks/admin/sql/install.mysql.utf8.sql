7a212e5498780107776e8d06a480400ccee697eb
rogercreagh <44980188+rogercreagh@users.noreply.github.com>
2022-12-13 20:56:43 +0000

    Update install.mysql.utf8.sql

--- src/com_xbbooks/admin/sql/install.mysql.utf8.sql
+++ src/com_xbbooks/admin/sql/install.mysql.utf8.sql
@@ -1,4 +1,4 @@
-# sql installation file for component xbBooks v0.9.9.8 21st October 2022
+# sql installation file for component xbBooks v1.0.0.1 13th December 2022
 # NB no book data is installed with this file, default categories are created by the installation script
 
 INSERT INTO `#__content_types` (`type_title`, `type_alias`, `content_history_options`, `table`, `field_mappings`, `router`,`rules`) 
@@ -24,50 +24,6 @@ VALUES
     "core_catid": "catid"
   }}',
 'XbbooksHelperRoute::getBookRoute',''),
-
--- ('Xbbooks Person', 'com_xbbooks.person', 
--- '{"formFile":"administrator\\/components\\/com_xbbooks\\/models\\/forms\\/person.xml", 
---     "hideFields":["checked_out","checked_out_time"], 
---     "ignoreChanges":["checked_out", "checked_out_time"],
---     "convertToInt":[], 
---     "displayLookup":[
---         {"sourceColumn":"catid","targetTable":"#__categories","targetColumn":"id","displayColumn":"title"}
---     ]
---  }',
--- '{"special":{"dbtable":"#__xbpersons","key":"id","type":"Person","prefix":"XbbooksTable","config":"array()"},
---     "common":{"dbtable":"#__ucm_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}',
--- '{"common": {
---     "core_content_item_id": "id",
---     "core_title": "lastname",
---     "core_state": "state",
---     "core_alias": "alias",
---     "core_created_time": "created",
---     "core_body": "biography",
---     "core_catid": "catid"
---   }}',
--- 'XbbooksHelperRoute::getPersonRoute',''),
--- 
--- ('Xbbooks Character', 'com_xbbooks.character', 
--- '{"formFile":"administrator\\/components\\/com_xbbooks\\/models\\/forms\\/character.xml", 
---     "hideFields":["checked_out","checked_out_time"], 
---     "ignoreChanges":["checked_out", "checked_out_time"],
---     "convertToInt":[], 
---     "displayLookup":[
---         {"sourceColumn":"catid","targetTable":"#__categories","targetColumn":"id","displayColumn":"title"}
---     ]
---  }',
--- '{"special":{"dbtable":"#__xbcharacters","key":"id","type":"Character","prefix":"XbbooksTable","config":"array()"},
---     "common":{"dbtable":"#__ucm_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}',
--- '{"common": {
---     "core_content_item_id": "id",
---     "core_title": "name",
---     "core_state": "state",
---     "core_alias": "alias",
---     "core_created_time": "created",
---     "core_body": "description",
---     "core_catid": "catid"
---   }}',
--- 'XbbooksHelperRoute::getCharacterRoute',''),
 
 ('Xbbooks Review', 'com_xbbooks.review', 
 '{"formFile":"administrator\\/components\\/com_xbbooks\\/models\\/forms\\/review.xml", 
@@ -138,65 +94,6 @@ VALUES
 	"note":"note"}}',
 'XbbooksHelperRoute::getCategoryRoute','');
 
-CREATE TABLE IF NOT EXISTS `#__xbpersons` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `asset_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `firstname` varchar(190) NOT NULL DEFAULT '',
-  `lastname` varchar(190) NOT NULL DEFAULT '',
-  `alias` varchar(190) NOT NULL DEFAULT '',
-  `summary` varchar(255) NOT NULL DEFAULT '',
-  `biography` mediumtext,
-  `portrait` mediumtext NOT NULL DEFAULT '',
-  `nationality` varchar(100) NOT NULL DEFAULT '',
-  `year_born` smallint NULL,
-  `year_died` smallint NULL,
-  `ext_links` mediumtext,
-  `catid` int(10) NOT NULL  DEFAULT '0',
-  `access` int(10) NOT NULL  DEFAULT '0',
-  `state` tinyint(3) NOT NULL DEFAULT '0',
-  `created` datetime,
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `created_by_alias` varchar(255) NOT NULL DEFAULT '',
-  `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
-  `checked_out_time` datetime,
-  `modified` datetime,
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `metadata` mediumtext NOT NULL DEFAULT '',
-  `ordering` int(10) NOT NULL DEFAULT '0',
-  `params` mediumtext NOT NULL DEFAULT '',
-  `note` mediumtext,
-  PRIMARY KEY (`id`)
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
-
-# CREATE UNIQUE INDEX `personaliasindex` ON `#__xbpersons` (`alias`);
-
-CREATE TABLE IF NOT EXISTS `#__xbcharacters` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `asset_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `name` varchar(100) NOT NULL DEFAULT '',
-  `alias` varchar(190) NOT NULL DEFAULT '',
-  `summary` varchar(255) NOT NULL DEFAULT '',
-  `description` mediumtext,
-  `image` mediumtext NOT NULL DEFAULT '',
-  `catid` int(10) NOT NULL  DEFAULT '0',
-  `access` int(10) NOT NULL  DEFAULT '0',
-  `state` tinyint(3) NOT NULL DEFAULT '0',
-  `created` datetime,
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `created_by_alias` varchar(255) NOT NULL DEFAULT '',
-  `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
-  `checked_out_time` datetime,
-  `modified` datetime,
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `metadata` mediumtext NOT NULL DEFAULT '',
-  `ordering` int(10) NOT NULL DEFAULT '0',
-  `params` mediumtext NOT NULL DEFAULT '',
-  `note` mediumtext,
-  PRIMARY KEY (`id`)
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
-
-# CREATE UNIQUE INDEX `characteraliasindex` ON `#__xbcharacters` (`alias`);
-
 CREATE TABLE IF NOT EXISTS `#__xbbooks` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `asset_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -256,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `#__xbbookcharacter` (
   `listorder` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `idx_book_id` (`book_id`),
-  KEY `idx_person_id` (`char_id`)
+  KEY `idx_char_id` (`char_id`)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `#__xbbookreviews` (
